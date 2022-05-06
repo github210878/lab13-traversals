5515e9f3981352c7e83276c1d7f60995d1fb692c
Isa Pudiyapura <isa.pudiyapura@gmail.com>
2022-05-06 15:03:06 -0700

    inOrder implemented

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -142,7 +142,15 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	}
 	
 	public void inOrderRecurse(BSTNode<T> node) {
-		
+		if(node.leftChild != null)
+		{
+			inOrderRecurse(node.leftChild);
+		}
+		System.out.print(node.data + " ");
+		if(node.rightChild != null)
+		{
+			inOrderRecurse(node.rightChild);
+		}
 	}
 	//Traverse the tree in an inorder fashion but using a stack
 	public void inOrderStack() {
